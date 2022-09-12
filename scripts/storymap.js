fe63989a63a7ed8107d7411d2d96c1ab64fd0ca0
Hana Nan <38077529+aulia2695@users.noreply.github.com>
2022-09-13 00:26:34 +0700

    Update position control search

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -279,7 +279,7 @@ $(window).on('load', function() {
     
     //search control option
     var controlSearch = new L.Control.Search({
-      position:'topleft',	
+      position:'topcenter',	
       // style: 'bar',
       sourceData: localData,
       initial: false,
