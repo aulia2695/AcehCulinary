3e3384dd9ef2e01dac7c17038876354c0ffdfdab
Hana Nan <38077529+aulia2695@users.noreply.github.com>
2022-09-13 13:23:25 +0700

    Update strotymap.js
    change position search control option

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -279,7 +279,7 @@ $(window).on('load', function() {
     
     //search control option
     var controlSearch = new L.Control.Search({
-      position:'topleft',		
+      position:'topright',		
       sourceData: localData,
       initial: false,
       // zoom: 16,
